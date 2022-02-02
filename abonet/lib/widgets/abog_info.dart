825f4de5db20cc7686a2a8df2b170b2481f8847b
jaimepizarr <japibaro@espol.edu.ec>
2022-02-02 03:00:23 -0500

    Comment box

--- abonet/lib/widgets/abog_info.dart
+++ abonet/lib/widgets/abog_info.dart
@@ -11,40 +11,43 @@ class AbogInfo extends StatelessWidget {
     final apiService = Provider.of<ApiService>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Perfil"),
+        title: Text("Abogado"),
       ),
-      body: FutureBuilder<Map<String, dynamic>>(
-          future: apiService.getAbogadoById(abogadoId),
-          builder: (BuildContext context,
-              AsyncSnapshot<Map<String, dynamic>> snapshot) {
-            if (snapshot.hasData) {
-              return MainContainer(snapshot.data!);
-              ;
-            } else if (snapshot.hasError) {
-              return Column(
-                children: [
-                  const Icon(
-                    Icons.error_outline,
-                    color: Colors.red,
-                    size: 60,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text('Error: ${snapshot.error}'),
-                  )
-                ],
-              );
-            } else {
-              return Center(
-                child: SizedBox(
-                  width: 60,
-                  height: 60,
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }
-          }),
+      body: futureWidget(apiService),
     );
+  }
+
+  FutureBuilder<Map<String, dynamic>> futureWidget(ApiService apiService) {
+    return FutureBuilder<Map<String, dynamic>>(
+        future: apiService.getAbogadoById(abogadoId),
+        builder: (BuildContext context,
+            AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          if (snapshot.hasData) {
+            return MainContainer(snapshot.data!);
+          } else if (snapshot.hasError) {
+            return Column(
+              children: [
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                )
+              ],
+            );
+          } else {
+            return Center(
+              child: SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
   }
 }
 
@@ -56,13 +59,10 @@ class MainContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(children: [
-        Text("${data["nombre_completo"]}",
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-        Icon(Icons.account_box, size: 120),
-        listProfAttrs(data)
-      ])),
+      body: ListView(
+        shrinkWrap: true,
+        children: [listProfAttrs(data), writeComment()],
+      ),
     );
   }
 
@@ -72,6 +72,9 @@ class MainContainer extends StatelessWidget {
         ? data["categoria"].map((cat) => cat["nombre"]).toList().join(", ")
         : "";
     return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      Text("${data["nombre_completo"]}",
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+      Icon(Icons.account_box, size: 120),
       profAttrUniq(
           "Ciudad",
           data.containsKey("ubicacion") && data["ubicacion"].length > 0
@@ -100,5 +103,60 @@ class MainContainer extends StatelessWidget {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
           Text("$value", style: TextStyle(fontSize: 16)),
         ]));
+  }
+
+  Widget writeComment() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: EdgeInsets.only(left: 20),
+        child: Text("Opiniones",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+      ),
+      Card(
+          child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.account_circle_rounded, size: 50),
+                        SizedBox(width: 10),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Yo",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold)),
+                              cuatroEstrellas()
+                            ]),
+                      ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    Expanded(
+                      child: TextField(
+                        maxLines: 2,
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Comentario',
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                        padding: EdgeInsets.all(0),
+                        icon: Icon(Icons.send),
+                        onPressed: () {})
+                  ])
+                ],
+              )))
+    ]);
+  }
+
+  Widget cuatroEstrellas() {
+    return Row(
+        children: List.filled(4, Icon(Icons.star_outlined)) +
+            [Icon(Icons.star_outline)]);
   }
 }
